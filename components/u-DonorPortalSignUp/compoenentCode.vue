71ec431757ab293a8ae48716f53d954b00b2ca37
Dev-Betterverse <null>
2022-06-09 16:06:24 +0000

    - testing for first commit - added custom components for SVG icons - started refactoring registration page

---
+++ components/u-DonorPortalSignUp/compoenentCode.vue
@@ -0,0 +1,270 @@
+<!-- Donor Portal Sign Up Form -->
+
+<template>
+  <div class="w-100">
+
+    <!-- Header for small displays -->
+    <b-container class="d-block d-lg-none w-100">
+      <b-row id="status-bar-clearance"></b-row>
+
+      <b-row class="mt-1 mb-5">
+        <b-col class="d-flex align-items-center">
+          <icon-betterverse id="md-logo"/>
+        </b-col>
+        
+        <b-col class="text-right">
+          <small>Have an account?<br><a href="#">Log in</a></small>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <!-- Main Header -->
+    <div class="my-2">Create Account</div>
+    <div class="my-3 mb-4">
+      <slot>
+        <h1>Welcome to the <b>Betterverse</b></h1>
+        <h2>Here will say something about this and how it must be extraordinay.</h2>
+      </slot>
+    </div>
+
+    <!-- Form -->
+    <form @submit.prevent="onSubmit">
+
+      <!-- E-mail Input -->
+      <div class="field-wrapper">
+        <label for="email-input">Continue with email</label>
+        <div class="input-wrapper">
+          <span>
+            <icon-mail class="prefix-icon" />
+          </span>
+          <input
+            v-model="email"
+            type="text"
+            id="email-input"
+            name="email-input"
+            autocomplete="on"
+            placeholder="Type e-mail address"
+          />
+          <span>
+            <icon-checkmark />
+          </span>
+        </div>
+        <div class="error-div">
+          <small v-for="err in emailErrors" class="error-message"
+            >{{ err }}<br
+          /></small>
+        </div>
+      </div>
+
+      <!-- Password Input -->
+      <div class="field-wrapper">
+        <label for="password-input">Password</label>
+        <div class="input-wrapper">
+          <span>
+            <icon-lock class="prefix-icon" />
+          </span>
+          <input
+            v-model="password"
+            :type="inputType"
+            id="password-input"
+            name="password"
+            autocomplete="new-password"
+            placeholder="Type password"
+          />
+          <span @click="togglePasswordVisibility">
+            <icon-eye
+              :shown="inputType === 'text'"
+              class="suffix-icon"
+            />
+          </span>
+        </div>
+        <div class="error-div">
+          <small v-for="err in passwordErrors" class="error-message"
+            >{{ err }}<br
+          /></small>
+        </div>
+      </div>
+
+      <!-- Continue button -->
+      <div class="button-wrapper d-flex justify-content-end">
+        <button id="continue-email-button">
+          Continue with email
+          <span id="continue-email-arrow"><icon-leftarrow /></span>
+        </button>
+      </div>
+
+    </form>
+  </div>
+</template>
+
+<script>
+const DEFAULT_TIMEOUT = 6500 // in miliseconds
+
+module.exports = {
+  data() {
+    return {
+      password: '',
+      email: '',
+      confirmPass: '',
+      inputType: 'password',
+      emailErrors: [],
+      passwordErrors: [],
+      confirmPassErrors: []
+    }
+  },
+
+  methods: {
+    // events
+
+    onSubmit(e) {
+      this.displayError(this.passwordErrors, 'Invalid password')
+    },
+
+    // functionality
+
+    togglePasswordVisibility() {
+      if (this.inputType === 'password') this.inputType = 'text'
+      else this.inputType = 'password'
+      console.log(`toggled to: ${this.inputType}`)
+    },
+
+    displayError(errMsgArray, message) {
+      if (errMsgArray.includes(message)) return
+
+      errMsgArray.push(message)
+
+      setTimeout(() => {
+        let index = errMsgArray.indexOf(message)
+        errMsgArray.splice(index, 1)
+      }, DEFAULT_TIMEOUT)
+    },
+
+    clearErrors(errMsgArray) {
+      errMsgArray.splice(0, errMsgArray.length)
+    }
+  },
+
+  components: {
+    IconCheckMark: $getCustomComponent('u-Icons-Checkmark'),
+    IconEye: $getCustomComponent('u-Icons-Eye'),
+    IconLeftarrow: $getCustomComponent('u-Icons-Leftarrow'),
+    IconLock: $getCustomComponent('u-Icons-Lock'),
+    IconMail: $getCustomComponent('u-Icons-Mail'),
+    IconBetterverse: $getCustomComponent('u-Icons-Betterverse')
+  }
+}
+</script>
+
+<style>
+body {
+  font-size: 14px;
+  font-weight: 500;
+}
+
+h1 {
+  font-family: 'BN Cringe Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 45px;
+  line-height: 48px;
+}
+
+h2 {
+  font-family: 'BN Cringe Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+}
+
+.container, .row, .col {
+  padding: 0;
+  margin: 0;
+}
+
+#status-bar-clearance {
+  height: 44px;
+}
+
+#md-logo {
+  heigth: 32px;
+  width: 32px;
+}
+
+.field-wrapper,
+.button-wrapper {
+  padding: 8px 0;
+}
+
+.input-wrapper {
+  height: 32px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+}
+
+.prefix-icon {
+  height: 20px;
+  width: 20px;
+}
+
+label {
+  font-size: 12px;
+}
+
+input {
+  width: 100%;
+  text-indent: 5px;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.suffix-icon {
+  height: 20px;
+  width: 20px;
+}
+
+.error-message {
+  color: #e15564;
+}
+
+.error-div {
+  padding: 0.5em 0;
+}
+
+#continue-email-button {
+  color: white;
+  background: black;
+
+  height: 32px;
+  padding: 6px 8px 6px 12px;
+  gap: 4px;
+
+  border: 1px solid #000000;
+  border-radius: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+::placeholder,
+::-webkit-input-placeholder,
+:-ms-input-placeholder {
+  color: #000000;
+  opacity: 0.5;
+}
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+  transition: background-color 5000s ease-in-out 5000s;
+}
+</style>
---
+++ components/u-Icons-Checkmark/compoenentCode.vue
@@ -0,0 +1,53 @@
+<script>
+module.exports = {
+  data() {
+    return {
+      hidden: Boolean,
+      isvalid: Boolean
+    }
+  }
+}
+</script>
+
+<template>
+  <svg
+    v-if="isvalid && !hidden"
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <circle cx="10" cy="10" r="8" fill="#25C791" />
+    <path
+      d="M6.83309 10.5L8.99976 12.5L13.3331 7.5"
+      stroke="white"
+      stroke-width="1.3"
+    />
+  </svg>
+
+  <svg
+    v-else-if="!isvalid && !hidden"
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <path
+      d="M10 4C14.5 4 17 9 17 10C17 11 14.5 16 10 16C5.5 16 3 11 3 10C3 9 5.5 4 10 4Z"
+      stroke="black"
+      stroke-width="1.2"
+    />
+    <circle cx="10" cy="10" r="3" stroke="black" stroke-width="1.2" />
+    <line
+      x1="4.84853"
+      y1="5"
+      x2="15"
+      y2="15.1515"
+      stroke="black"
+      stroke-width="1.2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
