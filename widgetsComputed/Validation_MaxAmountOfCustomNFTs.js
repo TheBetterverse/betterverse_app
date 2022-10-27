21aacb4fb26b1113fefe7ee33acdfa4a8063605c
Dev-Betterverse <null>
2022-10-27 21:21:44 +0000

    Slide transaction test prep

--- widgetsComputed/Validation_MaxAmountOfCustomNFTs.js
+++ widgetsComputed/Validation_MaxAmountOfCustomNFTs.js
@@ -4,6 +4,7 @@ return () => {
     let donation = this.DonorPortal_GetDonationAmountNumber()
     let max = Math.floor(donation / 10)
 
+    console.log('MAX NFT COUNT: ' + max)
     if (max > 10) max = 10
 
     return max
